5570e2d04801918b0fa3ffc7c156e9f4a8dd9903
fedor <fsvistelnik@gmail.com>
2026-05-25 16:45:23 +0300

    fix button paddding message screen

--- app/src/main/java/com/example/flat_rent_app/presentation/screens/chatscreen/ChatScreen.kt
+++ app/src/main/java/com/example/flat_rent_app/presentation/screens/chatscreen/ChatScreen.kt
@@ -342,7 +342,9 @@ fun ChatScreenContent(
     ) { pad ->
         Box(modifier = Modifier
             .padding(pad)
-            .fillMaxSize()) {
+            .fillMaxSize()
+            .imePadding()
+        ) {
             LazyColumn(
                 state = listState,
                 modifier = Modifier
